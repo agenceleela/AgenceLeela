5acd89594712e50e5bc64911a7bfe8b66a3e75d6
agenceleela <agenceleela@gmail.com>
2026-09-17 14:17:10 +0200

    feat: social proof pré-formulaire - slots places fondatrices (rareté honnête)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import SocialProof from '../components/SocialProof'
+import FounderSlots from '../components/FounderSlots'
 import LeadForm from '../components/LeadForm'
 import FAQ from '../components/FAQ'
 import Footer from '../components/Footer'
@@ -11,6 +12,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <SocialProof />
+      <FounderSlots />
       <LeadForm />
       <FAQ />
       <Footer />
